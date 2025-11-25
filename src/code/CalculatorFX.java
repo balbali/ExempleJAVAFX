ceb4f74bef9ea41603961c5ec1a2b400d47df860
Balbali <balbalifoued@gmail.com>
2025-11-25 10:26:26 +0100

    First commit calculatrice

---
+++ src/code/CalculatorFX.java
@@ -0,0 +1,27 @@
+package code;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalculatorFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Chemin relatif au package 'code'
+        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Calculatrice");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
